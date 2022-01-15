0007020c88c9557b684880de3a9dcafd842ad2d4
Colton Brugger <colton@coltonbrugger.com>
2022-01-15 10:01:42 -0600

    First Step

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -12,58 +12,130 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.photo}>
+          <img src="/lowell-brugger.jpeg" alt="Lowell Julian Brugger" />
+        </div>{' '}
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <span style={{ fontSize: '1rem', textTransform: 'uppercase' }}>
+            Remembering
+          </span>
+          <br />
+          Lowell Julian Brugger
         </h1>
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          December 8, 1936 - December 25, 2021
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
+        <div>
+          <p>
+            On December 25, 2021, Christmas Day afternoon, Lowell Julian
+            Brugger, 85, died at his home in La Grange, Texas surrounded by his
+            loving family. He was known to many as “Buddy” Brugger, and lovingly
+            called “Papa” by his eleven grandchildren and nine
+            great-grandchildren.
+          </p>
+          <p>
+            The youngest of 4 children, Lowell was born to Johnny and Norma
+            Brugger in La Grange, Texas. He grew up between the farm in Halsted
+            and their home in La Grange.
+          </p>
+          <p>
+            Lowell married Rae in Mexico, honeymooned with her on a journey to
+            Alaska, and enjoyed much travel across the States with his wife,
+            children, and grandchildren. His career and outdoorsmen hobbies were
+            strongly rooted in Texas.
+          </p>
+          <p>
+            Lowell had worked in Construction since his teens. An accomplished
+            and respected craftsman, Lowell built homes, apartments, office
+            buildings and cabins with an artistic eye all over Texas. In and
+            around Fayette County, he has a great many satisfied customers.
+          </p>
+          <p>
+            When taking a short break from work, he could religiously be found
+            visiting with friends and any other passersby at the Donut Shop in
+            the mornings or at the ‘Dairy Mart’ (later DQ) in the afternoons.
+          </p>
+          <p>
+            On his longer breaks from work, he’d often be traveling to hunt for
+            his big buck out in West Texas, fishing at a nearby lake, or fishing
+            from the beach or pier along the Gulf coast. It was with photos and
+            trophies from all his hunting, fishing, and other trips that Papa
+            filled his game room. His game room will long remain the best place
+            for family and friends to look back on those cherished memories.
+          </p>
+          <p>
+            Lowell is survived by his wife of 58 years, Rae Elizabeth (Coenen)
+            Brugger; four children: Sheri Schneider and her husband, Glenn Ray
+            Schneider; Staci Brugger; Victoria Brugger; and Mark Brugger;
+            sister, Nellie Havins; eleven grandchildren: Brandon Schneider;
+            Colton Brugger and his wife, Allison; Lathan Staal and his wife,
+            Caitlyn; Samantha Brugger; Savannah Gotsch and her husband, David;
+            Dalton Staal; Sabrina Brugger; Shayleigh Staal; Sidney Brugger;
+            Hunter Brugger and friend, Jessica Recek; and Coy Magness; nine
+            great-grandchildren: Zoey Staal; Skylar Wallace; Lillian Brugger;
+            Avery Wallace; Ruth Brugger; Lowell Brugger; RaeLynn Brugger;
+            Thaddeus Gotsch; John Brugger; with number ten on the way; His
+            cousin’s widow, Loretta Brugger; many loving nephews: Glenn W.
+            Michalk and wife, Judy; Ronny Havins; Don Havins; Danny Dildy; Mike
+            Brugger and wife, Maxine; and Garry Brugger and wife, Deborah; along
+            with numerous other loving aunts, uncles, cousins, nieces, nephews,
+            and friends.
+          </p>
+          <p>
+            Lowell was preceded in death by his: parents Johnny and Norma;
+            uncle, Alfred Brugger and his wife, Esther; brother, John Brugger;
+            sister, Dorothy Dildy; nephew, Dwight Brugger; niece, Debbie
+            Cheshire and her husband, Robert; cousin, Roy Gene Brugger; and
+            granddaughter, Hollie Schneider.
+          </p>
+          <p>
+            A celebration of Lowell’s life will take place at the Fayette County
+            Fair Event Center on Saturday, January 15th from 11am to 2pm.
+            Lowell’s family and friends are invited to attend this celebration.
+            For more information about this event, please contact Colton Brugger
+            at 979-966-7995 or colton@coltonbrugger.com. Fayette County Fair
+            Event Center. 400 W Fair Grounds Rd, La Grange, TX 78945
+          </p>
+          <p>
+            In lieu of flowers, memorial contributions may be made in memory of
+            Lowell to:
+          </p>
+          <div style={{ paddingLeft: '1rem' }}>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Mount Calvary Lutheran Church
+              <br />
+              800 N Franklin St, La Grange, Texas 78945
+              <br />
+              mtcalvary-lcms.org
             </p>
-          </a>
+            <p>
+              Sacred Heart Catholic Church
+              <br />
+              539 E Pearl St, La Grange, Texas 78945
+              <br />
+              sacredheart-lg.org.
+            </p>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <h3>Obituary</h3>
+        <p>
+          <a href="https://www.lagrangefunerals.com/obituary/lowell-brugger">
+            View online obituary and sign the guest book here.
+          </a>
+        </p>
+
+        <h3>Slideshow</h3>
+        <p>
+          View the slideshow / photos from the celebration of life in{' '}
+          <a href="https://drive.google.com/drive/folders/1Znjb53PmJeMXnFawYSn_FUh5j5WB-iBA?usp=sharing">
+            this Google Drive folder
+          </a>
+          .
+        </p>
       </footer>
     </div>
-  )
+  );
 }
